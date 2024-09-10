b8423553e65f8a40a657ef7678a1f484f12b0a85
Subham Saha <subhamsahadeep@gmail.com>
2024-09-10 19:10:28 +0530

    added vercel Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import './globals.css';
 import './scrollbar.css';
 import { SpeedInsights } from '@vercel/speed-insights/next';
+import { Analytics } from '@vercel/analytics/react';
 
 export const metadata: Metadata = {
   title: 'Subham Saha - Senior Software Engineer',
@@ -19,6 +20,7 @@ export default function RootLayout({
       <body>
         {children}
         <SpeedInsights />
+        <Analytics />
       </body>
     </html>
   );
