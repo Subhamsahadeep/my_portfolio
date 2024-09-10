20db43a63c7693eda21248349fd2adbb01255376
Subham Saha <subhamsahadeep@gmail.com>
2024-09-10 12:58:03 +0530

    added speed insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from 'next';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 import './globals.css';
 import './scrollbar.css';
 
@@ -15,6 +16,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>{children}</body>
+      <SpeedInsights />
     </html>
   );
 }
